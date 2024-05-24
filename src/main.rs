6bb42d7c1659ba53fd83b9a826e147158042a96e
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2024-05-23 21:32:27 -0300

    add tracing for log messages from services

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,13 @@ use futures_util::StreamExt;
 use rand::Rng;
 use std::{
     collections::HashSet,
+    net::Ipv4Addr,
     sync::{Arc, Mutex},
 };
+use tower_http::trace::TraceLayer;
+
+use tracing::{error, info};
+use tracing_subscriber::{self, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Clone)]
 struct AppState {
@@ -22,6 +27,14 @@ struct AppState {
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "bigly=debug,tower_http=debug,axum::rejection=trace".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app_state = AppState {
         users: Arc::new(Mutex::new(HashSet::new())),
     };
@@ -29,15 +42,19 @@ async fn main() -> Result<()> {
     let app = Router::new()
         .route("/", get(index))
         .route("/chat", get(chat))
-        .with_state(app_state);
+        .with_state(app_state)
+        .layer(TraceLayer::new_for_http());
 
-    let listener = tokio::net::TcpListener::bind(("127.0.0.1", 3030)).await?;
+    let socket_addr = (Ipv4Addr::new(127, 0, 0, 1), 3030);
+    let listener = tokio::net::TcpListener::bind(socket_addr).await?;
+    info!("listening at address {}", listener.local_addr()?);
     axum::serve(listener, app).await?;
     Ok(())
 }
 
 async fn index() -> Html<&'static str> {
-    Html("
+    Html(
+        "
         <!DOCTYPE html>
         <html>
         <head>
@@ -70,7 +87,8 @@ async fn index() -> Html<&'static str> {
             </script>
         </body>
         </html>
-    ")
+        ",
+    )
 }
 
 async fn chat(ws: WebSocketUpgrade, State(state): State<AppState>) -> Response {
@@ -84,6 +102,7 @@ async fn handle_chat_websocket(ws: WebSocket, state: AppState, user_id: u64) {
 
     // Add the new user to the set of connected users
     state.users.lock().unwrap().insert(user_id);
+    info!("successful connection from uid={user_id}");
 
     // Loop to receive messages from the WebSocket connection
     while let Some(result) = receiver.next().await {
@@ -94,8 +113,7 @@ async fn handle_chat_websocket(ws: WebSocket, state: AppState, user_id: u64) {
                 }
             }
             Err(e) => {
-                // If there's an error, log it and break the loop
-                eprintln!("websocket error(uid={}): {}", user_id, e);
+                error!(error = e.to_string(), "websocket error from uid={user_id}");
                 break;
             }
         }
@@ -103,10 +121,12 @@ async fn handle_chat_websocket(ws: WebSocket, state: AppState, user_id: u64) {
 
     // Remove the user from the set of connected users when they disconnect
     state.users.lock().unwrap().insert(user_id);
+    info!("successful disconnection from uid={user_id}");
 }
 
 // Function to broadcast a message to all connected users except the sender
 async fn broadcast_message(user_id: u64, _msg: String, state: &AppState) {
+    info!("broadcasting message from uid={user_id}");
     for &uid in state.users.lock().unwrap().iter() {
         // Skip the sender of the message
         if uid != user_id {
