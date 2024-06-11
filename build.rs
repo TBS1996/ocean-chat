e3f43bcd98ebdc95d78f32bbca268bb87b6aaaed
neeleshpoli <72574589+neeleshpoli@users.noreply.github.com>
2024-06-11 16:39:51 -0500

    Added some missing files to build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,13 @@
 use std::{fs::File, path::Path};
 
 fn main() {
-    let path = Path::new("config.toml");
+    create_file("config.toml".to_string());
+    create_file("./files/dist".to_string());
+    create_file("./files/scores".to_string());
+}
+
+fn create_file(file_path: String) {
+    let path = Path::new(&file_path);
 
     if !path.exists() {
         File::create(path).unwrap();
